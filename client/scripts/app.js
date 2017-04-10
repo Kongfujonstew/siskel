5ebac0939cbbb5575c49e158fe2e9f65cc76a8a0
Kongfujonstew <jonstew@gmail.com>
2017-04-10 14:39:14 -0700

    first draft

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,14 +15,19 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change:like', function() {
+      this.sort();
+    }, this)
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
-  }
+    this.set('comparator', field);
+    console.log(field, this.comparator);
+  },
+
 
 });
 
@@ -58,7 +63,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render());
   },
 
   events: {
